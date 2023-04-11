bb203d1a1297da90f37e4c6dcb8c20bf7e46b871
achaidez <chaidez62@gmail.com>
2023-04-11 16:26:57 -0600

    EC2 updatesq

--- server/queries.js
+++ server/queries.js
@@ -232,5 +232,61 @@ module.exports = {
   postReview,
   markHelpful,
   markReported,
-  pool
+  setupTable
+}
+
+async function setupTable(pool) {
+  let createReviewTableQuery = `
+    CREATE TABLE IF NOT EXISTS review(
+      id BIGINT PRIMARY KEY NOT NULL ,
+      product_id INT,
+      rating INT,
+      date VARCHAR,
+      summary VARCHAR,
+      body VARCHAR,
+      recommend BOOLEAN,
+      reported BOOLEAN,
+      reviewer_name VARCHAR,
+      reviewer_email VARCHAR,
+      response VARCHAR,
+      helpfulness INT
+    );
+  `;
+
+  let createPhotoTableQuery = `
+    CREATE TABLE IF NOT EXISTS photo(
+      photo_id SERIAL PRIMARY KEY NOT NULL,
+      review_id INT,
+      url VARCHAR
+    );
+  `;
+
+  let createCharacteristicTableQuery = `
+    CREATE TABLE IF NOT EXISTS characteristic(
+      id SERIAL PRIMARY KEY NOT NULL ,
+      product_id INT,
+      name VARCHAR
+    );
+  `;
+
+  let createCharacteristicReviewsTableQuery = `
+    CREATE TABLE IF NOT EXISTS characteristicReviews(
+      id SERIAL PRIMARY KEY NOT NULL ,
+      characteristic_id INT,
+      review_id INT,
+      value INT
+    );
+  `;
+
+  let populateReviewsTable = `
+    COPY review
+    FROM '/Users/homeac/Downloads/reviews.csv'
+    DELIMITER ','
+    CSV HEADER;
+  `
+
+  return createReviewTableQuery;
+  return createPhotoTableQuery;
+  return createCharacteristicTableQuery;
+  return createCharacteristicReviewsTableQuery;
 }
